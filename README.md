0c4c81fee5026126958f190a2a6ce65d916fa5a9
lionneclement <52818512+lionneclement@users.noreply.github.com>
2019-11-04 19:49:03 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Bot_amazon_get_Brand
- 
+
+Le programme va récupérer tout les marques avec leur nombre de produit.
+Vous pouvez ajouter des catégorie et aussi changer le filtre pour le nombre de produit.
+
+Pour ajouter un lien dans 'categorie' il suffit d'aller dans une catégorie amazon de sélectionner 'Voir plus' dans 'Marque' et de récupérer le lien. 
+!Attention! Par défault 'affichage' est sélectionner sur 'Meilleures marques' et doit le rester pour que cela fonctionne.
+
+Pour modifier le filtre il suffit de trouver la function 'data.filter' et de changer le nombre.
+
+Par default 'categorie' à l'url de High-Tech.
+
+dealimited.fr
--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const puppeteer = require('puppeteer');
 
+//function qui se lance automatiquement (function name(){})()
 (async function pampa(){
-  const browser = await puppeteer.launch({headless: false});
+
+//lancement de la page
+const browser = await puppeteer.launch({headless: false});
 const page = await browser.newPage();
+//Pour ajouter un lien dans 'categorie' il suffit d'aller dans une catégorie amazon de sélectionner 'Voir plus' dans 'Marque' et de récupérer le lien. 
+//!Attention! Par défault 'affichage' est sélectionner sur 'Meilleures marques' et doit le rester pour que cela fonctionne.
 var categorie=[
-  'https://www.amazon.fr/gp/search/other/ref=lp_13921051_sa_p_89?rh=n%3A13921051&bbn=13921051&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1572890388',
-  'https://www.amazon.fr/gp/search/other/ref=sr_in_-2_1?rh=i%3Acomputers%2Cn%3A340858031&bbn=340858031&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1572890968'
+  'https://www.amazon.fr/gp/search/other/ref=lp_13921051_sa_p_89?rh=n%3A13921051&bbn=13921051&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1572892457'
 ]
 
 var resultat=[],lettre;
 for(let y =0;y<categorie.length;y++){
   for(let i=0;i<27;i++){
+    //si i ==0 lettre var être égale à '%23' se qui correspond à '#' et après lettre va être égale à 'a' puis 'b' ...
     (i==0)?(lettre = '%23'):lettre =String.fromCharCode(96+i);
+    //ajoute de '&indexField='+lettre qui nous permet de changer de page avec les différente lettres
     await page.goto(categorie[y]+'&indexField='+lettre,{waitUntil: 'networkidle0'});
+    //$$eval nous permet de selectioner tout les items donc un item contient le nom de la marque et le nombre de produit
+    //QuerySelector va nour permmettre de récupérer le nom et le nombre de l'item
     const data = await page.$$eval('.a-list-item', pp => pp.map(p=>[p.querySelector('.refinementLink').innerText,Number((p.querySelector('.narrowValue').innerText).replace(/(\s)|(\))|(\()/g,''))]));
+    //La function filter nous permet de sélectionner que les marques qui ont plus de 1000 produit (sur l'exemple) 
     const all = await data.filter(function(item) {
       if (item[1] >= 1000){
           return item 
